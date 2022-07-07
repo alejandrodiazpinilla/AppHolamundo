6433da464efa4bb316ec9d332d8bdec6d496c553
alejandrodiazpinilla <diazjohn83@gmail.com>
2022-07-07 15:53:01 -0500

    Mi primera app movil

---
+++ android/app/src/main/kotlin/com/example/app_hola_mundo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_hola_mundo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Hola Mundo')),
+      ),
+      body: const Center(child: Text('Hola Mundo')),
+    )),
+  );
+}
